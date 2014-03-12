f1125dfe08eeefefc38add8ccadd00662bfa3ca4
barret-m <michel.barret@bull.net>
2014-03-12 14:50:27 +0100

    Begin of ui prototyping with bootstrap

--- lib/Choice/App.pm
+++ lib/Choice/App.pm
@@ -1,10 +1,31 @@
 package Choice::App;
+use strict;
+use warnings;
 use Dancer ':syntax';
+use Template;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+get '/choix/:hash' => sub {
+   "You make a choice !";
+};
+
+get '/admin/:hash?' => sub {
+# si pas de hash on liste les votes en cours
+   "Admin ".(param('hash')||"all")."!";
+};
+
+get '/propose' => sub {
+# on crée un choix
+   my @types = qw/doodle vote/;
+   foreach(@types) { debug $_ }
+   template 'propose', {
+      'types' => \@types
+   };
 };
 
 true;
